b3d3b7586af2c7d66d23a509c7ec8c4a53565849
EDUARDO ALVES MARTINS NETO <108354728+Eduardo6k@users.noreply.github.com>
2023-07-26 06:58:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 23 14:30:33 2023
 
+@author: Eduar
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 
+class Inputs(BaseModel):
+  id : str   
+  resource: str
+  user_id: int
+  topic: str
+  application_id: int
+  attempts: int
+  sent: str
+  received: str
+
+vendas = {
+    
+    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
+    2: {"item": "garrafa 2L", "preco_unitario": 15, "quantidade": 5},
+    3: {"item": "garrafa 750ml", "preco_unitario": 10, "quantidade": 5},
+    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5},
+}
 @app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+def home():
+    return "MINHA API ESTÁ NO AR"
+
+@app.get("/venda/{id_venda}")
+def venda_especifica(id_venda: int):
+    return vendas[id_venda]
+
+@app.post("/ml")
+def Ml(inputs: Inputs) -> str:
+    return str(inputs)
+    
+    
+    
