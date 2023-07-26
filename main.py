c74c6ab67c3c7f55006de74794c1faac508b9b7c
EDUARDO ALVES MARTINS NETO <108354728+Eduardo6k@users.noreply.github.com>
2023-07-26 20:51:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ Created on Sun Jul 23 14:30:33 2023
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import mysql.connector
+
 
 
 app = FastAPI()
@@ -26,10 +28,43 @@ def home():
     return "MINHA API ESTÁ NO AR"
 
 
-@app.post("/ml")
+ @app.post("/ml")
 def Ml(inputs: Inputs) -> str:
-   return str(inputs)
+    query = '''
+        INSERT INTO sua_tabela (id, resource, user_id, topic, application_id, attempts, sent, received)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    '''
+    args = (
+        inputs.id,
+        inputs.resource,
+        inputs.user_id,
+        inputs.topic,
+        inputs.application_id,
+        inputs.attempts,
+        inputs.sent,
+        inputs.received
+    )
+    execute_query(query, args)
+    return "Dados inseridos com sucesso no banco de dados."
+
  
+db_config = {
+    'user': 'root',
+    'password': '3CdSQWBdD70V8AYhHa1D',
+    'host': 'containers-us-west-165.railway.app',
+    'database': 'railway'
+}
+def execute_query(query, args=None):
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+    if args:
+        cursor.execute(query, args)
+    else:
+        cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
 
    
 
