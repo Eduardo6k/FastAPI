635841227de0db202e321831b7b04a5078b3d7f8
EDUARDO ALVES MARTINS NETO <108354728+Eduardo6k@users.noreply.github.com>
2023-07-26 22:44:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def home():
 @app.post("/ml")
 def Ml(inputs: Inputs) -> str:
     query = '''
-        INSERT INTO webhookML2 (id, resource, user_id, topic, application_id, attempts, sent, received)
+        INSERT INTO webhookMl2 (id, resource, user_id, topic, application_id, attempts, sent, received)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
     '''
     args = (
